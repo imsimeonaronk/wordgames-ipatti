5b7e8dcee757b143f18aaea465eaa71c0362c3dd
imsimeonaronk <simeonaronk@gmail.com>
2025-05-29 16:31:03 +0530

    update-storage

---
+++ src/utils/LocalStorage.tsx
@@ -0,0 +1,12 @@
+export function lsSetItem(key:string,value:string){
+    localStorage.setItem(key,value);
+}
+
+export function lsGetItem(key:string){
+    return localStorage.getItem(key);
+}
+
+
+export function lsRemoveItem(key:string){
+    localStorage.removeItem(key);
+}
