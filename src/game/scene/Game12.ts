0853432c9a87baa6ce8ad867696653cc86d2bfa9
imsimeonaronk <simeonaronk@gmail.com>
2025-08-12 11:00:21 +0530

    update

--- src/game/scene/Game12.ts
+++ src/game/scene/Game12.ts
@@ -1,6 +1,9 @@
-import { ssGetItem } from "../../utils/SessionStorage";
+import Sounds from "../libs/Sounds";
+import { GenerateTaskNumber, ResetFirstVisit, ShouldPromptLogin, UpdateScore } from "../utils/UserPlay";
+import Center from "../object/Center";
 import FpsText from "../object/FPS";
 import { Gvar } from "../utils/Gvar";
+import ExplodeParticle from "../utils/Particles";
 import { Scenes } from "../utils/Scenes";
 
 class Game12 extends Phaser.Scene{
@@ -8,6 +11,17 @@ class Game12 extends Phaser.Scene{
     private sceneClose: boolean = false;
     private sceneName: string = Scenes.Game12;
     private fpsText: FpsText | undefined;
+
+    private gameContainer: Phaser.GameObjects.Container | undefined;
+    private startGame: boolean = false;
+    private endGame: boolean = false;
+
+    private explodeParticle: ExplodeParticle | undefined;
+
+    private currentTask: number = 0;
+    private totalTask: number = 0;
+
+    private gameScore: number = 0;
 
     constructor(){
         super({
@@ -18,7 +32,6 @@ class Game12 extends Phaser.Scene{
     //Basic Function
     init(){
         this.initscene();
-        this.createscene();
         this.sceneevent();
     }
 
@@ -27,7 +40,17 @@ class Game12 extends Phaser.Scene{
     }
 
     create(){
-        console.log("Game12 scene created");
+        Gvar.consolelog("Game12 scene created");
+        window.Sounds = new Sounds(this);
+        window.Sounds.load('general');
+
+        this.createscene();
+
+        this.explodeParticle = new ExplodeParticle(this);
+        this.explodeParticle.init();
+
+        const center = new Center(this);
+
         this.fpsText = new FpsText(this);
     }
 
@@ -39,10 +62,53 @@ class Game12 extends Phaser.Scene{
     //Other Function
     private initscene(){
         this.sceneClose = false;
+        this.startGame = false;
+        this.endGame = false;
+        this.gameScore = 0;
+        this.currentTask = 0;
+        this.totalTask = 0;
     }
 
     private createscene(){
+        const data = this.cache.json.get("game-data");
+        const taskNumber = GenerateTaskNumber(Object.keys(data).length);
+        const taskData = data[`TASK${taskNumber}`];
         
+        //Task details
+        Gvar.consolelog("Task :"+taskNumber)
+        Gvar.consolelog(taskData)
+
+        this.currentTask = taskNumber;
+        this.totalTask = Object.keys(data).length;
+        
+        
+    }
+
+    private interactivelistener(flag:boolean){
+        
+    }
+
+    private checkresult(){
+        this.endGame = true;
+        this.gameScore ++; //Game Score increment
+        // Send score to firebase leaderboard
+        UpdateScore();
+        // Check for login
+        const loginPrompt = ShouldPromptLogin();
+        if(loginPrompt){
+            ResetFirstVisit();
+            if(window.openLoginBoard){
+                window.openLoginBoard();
+            }
+        }
+        //Load Next Task
+        setTimeout(()=>{
+            this.loadnexttask();
+        },500)
+    }
+
+    private loadnexttask(){
+        this.scene.restart();
     }
 
     private movetoscene(sceneName:string){
@@ -69,6 +135,7 @@ class Game12 extends Phaser.Scene{
     }
 
     private onsceneclear(){
+        this.explodeParticle?.clear();
         this.sound.stopAll();
     }
 
