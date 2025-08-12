c317db95d2703c9594282d7fa0fe0f2a3f085faa
imsimeonaronk <simeonaronk@gmail.com>
2025-08-12 12:21:08 +0530

    update

--- src/game/utils/Colors.ts
+++ src/game/utils/Colors.ts
@@ -8,6 +8,93 @@ export const Colors:any = {
         "Options-Box-Line": 0x000000
     },
     "Game-2":{
-        
+        "Sentence": "#595654",
+        "Empty": "#dbdb83",
+        "Empty-Text": "#697028",
+        "Word": "#ffffff",
+        "Options-Box": 0x656c25,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-3":{
+        "Image-Box": 0xffffff,
+        "Image-Box-Line": 0x000000,
+        "Word": "#ffffff",
+        "Options-Box": 0x3f7189,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-4":{
+        "Word": "#c62a39",
+        "Image-Frame": 0xededed,
+        "Image-Box": 0xffffff,
+        "Image-Box-Line": 0x000000
+    },
+    "Game-5":{
+        "Left-Box": 0x61aeba,
+        "Right-Box": 0xc66078,
+        "Text": 0x000000,
+        "Text-Highlight": 0xffffff,
+        "Record-Box": 0xc0c657,
+        "Record-Box-Line": 0x000000
+    },
+    "Game-6":{
+        "Sentence": "#595654",
+        "Empty": "#595654",
+        "Line": 0x595654,
+        "Word": "#ffffff",
+        "Options-Box": 0xa0a035,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-7":{
+        "Sentence": "#3f7189",
+        "Word": "#ffffff",
+        "Options-Box": 0xdb788c,
+        "Options-Box-Select": 0xb0131e,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-8":{
+        "Image-Box": 0xffffff,
+        "Image-Box-Line": 0x000000,
+        "Empty-Box": "#e08596",
+        "Empty-Box-Text": "#da7687",
+        "Word": "#ffffff",
+        "Options-Box": 0x656c25,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-9":{
+        "Wheel": 0x9ee2f2,
+        "Wheel-Line": 0x000000,
+        "Wheel-Center": 0xffffff,
+        "Wheel-Center-Line": 0x000000,
+        "Wheel-Center-Text": 0x666666,
+        "Wheel-Circle": 0x595654,
+        "Wheel-Circle-Line": 0x000000,
+        "Wheel-Circle-Text": "#ffffff",
+        "Word": 0x000000,
+    },
+    "Game-10":{
+        "Text-Box": 0x58b5c4,
+        "Text-Box-Correct": 0xc0c657,
+        "Text-Box-Wrong": 0xdb788c,
+        "Text-Box-Select": 0xc69e57,
+        "Text-Box-Line": 0x000000,
+        "Text-Box-Text": "#ffffff",
+        "Text-Box-Correct-Text": "#656c25",
+        "Text-Box-Wrong-Text": "#b41d29",
+        "Text-Box-Select-Text": "#654e26",
+        "Word": 0x000000,
+    },
+    "Game-11":{
+        "Text-Frame": "#e5e59c",
+        "Text-Line": "#656c25",
+        "Word": "#656c25",
+        "Options-Box": 0x595654,
+        "Options-Box-Line": 0x000000
+    },
+    "Game-12":{
+        "Board": "#58b5c4",
+        "Correct": "#c0c657",
+        "Text": "#ffffff",
+        "Options-Box": 0xdb788c,
+        "Options-Box-Line": 0x000000
     }
 }
